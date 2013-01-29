1718a924ee6527d9c1e4850f3b1e029218130c7a
Nong Li <nong@cloudera.com>
2013-01-29 15:50:23 -0800

    Updated readme and schema encoding.

--- src/thrift/redfile.thrift
+++ src/thrift/redfile.thrift
@@ -38,6 +38,39 @@ enum Type {
   BYTE_ARRAY = 6;
 }
 
+/** 
+ * Representation of Schemas
+ */
+enum SchemaFieldType {
+  /** This field is required (can not be null) **/
+  REQUIRED = 0;
+
+  /** The field is optional (can be null) **/
+  OPTIONAL = 1;
+
+  /** The field is repeated and can contain 0 or more values **/
+  REPEATED = 2;
+}
+
+/**
+ * Represents a element inside a schema definition.  
+ */
+struct SchemaElement {
+  /** Data type for this field. e.g. int32 **/
+  1: required Type type;
+
+  2: required SchemaFieldType field_type;
+
+  /** Name of the field in the schema **/
+  3: required string name;
+
+  /** Nested fields.  Since thrift does not support nested fields,
+   * the nesting if flattened to a single list.  These indices
+   * are used to construct the nested relationship
+   **/
+  4: required list<i32> children_indices;
+}
+
 /**
  * Encodings supported by redfile.  Not all encodings are valid for all types.
  */
@@ -119,7 +152,8 @@ struct ColumnMetaData {
   /** Type of this column **/
   1: required Type type
 
-  /** Set of all encodings used for this column. The purpose is to validate whether we can decode those pages. **/
+  /** Set of all encodings used for this column. The purpose is to validate 
+   * whether we can decode those pages. **/
   2: required list<Encoding> encodings
 
   /** Path in schema **/
@@ -178,16 +212,17 @@ struct FileMetaData {
   /** Version of this file **/
   1: required i32 version
 
-  /** Number of rows in this file **/
-  2: required i64 num_rows
+  /** Schema for this file.  The first element represents the root of schema;
+   * there is always a single root
+   */
+  2: required list<SchemaElement> schema;
 
-  /** schema for this file **/
-  3: required string schema
+  /** Number of rows in this file **/
+  3: required i64 num_rows
 
   /** Row groups in this file **/
   4: required list<RowGroup> row_groups
 
   /** Optional key/value metadata **/
   5: optional list<KeyValue> key_value_metadata
-
 }
